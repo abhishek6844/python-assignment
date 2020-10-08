652c64c0bc924489991aa44e61bda7ccb5bf0ce0
abhishek6844 <71318680+abhishek6844@users.noreply.github.com>
2020-10-08 15:22:56 +0530

    Add files via upload

---
+++ if else assignment.py
@@ -0,0 +1,9 @@
+#check the conditions
+#print("what is your age?")
+#age = int(input())
+#if age<18:
+ #   print("you can not drive")
+#elif age==18:
+ #   print("you come first and then we will decide")
+#else:
+ #   print("you can drive")
