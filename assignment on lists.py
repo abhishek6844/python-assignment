0f326d82b7b1efac364a4f3ab5bc4992a76b0c78
abhishek6844 <71318680+abhishek6844@users.noreply.github.com>
2020-10-08 14:56:49 +0530

    Add files via upload

---
+++ assignment on lists.py
@@ -0,0 +1,49 @@
+#list1 = [1,2,3,4,5,6]
+#list1.reverse()
+#list1.reverse()
+#print(list1)
+
+
+#sum of all the items in the list
+#l = [1,3,5,8,9,10]
+#total = sum(l)
+#print("sum of all elements in given list:",total)
+
+
+#largest number in list
+#list1= [1,3,4]
+#print(max(list1))
+
+
+#smallest number in list
+#list2= [1,4,5,8,0.4]
+#print(min(list2)
+
+
+#sort the list
+#abd = [(2,3),(3,4),(4,5),(8,9)]
+#abd.sort()
+#print(abd)
+
+#program to check if list is empty or not
+#anusha = [1,2,3]
+#if anusha == 0:
+  #  print("empty")
+#else:
+ #   print("not empty")
+
+
+ #program to copy a list
+#abhishek = [1,3,4,5,8]
+#x= abhishek.copy()
+#print(x)
+
+#program to specified list after removing oth , 4th, 5th element
+#boss = ['abd','bcg','rahul','bani','sushil','anusha']
+#boss.remove("abd")
+##boss.remove("sushil")
+#boss.remove("anusha")
+#print(boss)
+
+
+
