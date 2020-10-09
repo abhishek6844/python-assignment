77ac39092601c1310dc33ecd7c9ca2f645074b19
abhishek6844 <71318680+abhishek6844@users.noreply.github.com>
2020-10-09 21:50:28 +0530

    Add files via upload

---
+++ assignment on dictionary.py
@@ -0,0 +1,67 @@
+
+#to sort acending and decending a dictionary by value
+
+#numbers={1:20,2:30,3:40,4:50}
+
+
+
+
+#add a key to dictionary
+#abhishek = {1:30,2:30}
+#abhishek[3] = 60
+#print(abhishek)
+
+#comcemate the dictionary to form a nem one
+d1 = {1:30,2:20}
+d2 = {3:40,4:30}
+d3 = {2:30,3:60}
+myteam = {"d1":d1,"d2":d2,"d3":d3}
+print(myteam)
+
+#whether a key is already exists in dictionary
+#harry = {"python":3,"c":2,"java":5}
+#if "python" in harry:
+ #   print("present")
+#else:
+ #   print("not present")
+
+
+
+#take input from user and return the word from dictionary
+#d = {"mango":"fruit","banana":"carb fruit","orange":"juicy fruit"}
+#print("wlcome to fruit store")
+#d1 = input()
+#print(d[d1])
+
+
+#program to remove key from dictionary
+#d1 = {1:3,3:6,6:4,8:6}
+#print(len(d1))
+#del(d1[1])
+#print(d1)
+
+#to map twolist into a dictionary
+#abhi = ["electronics","physics","marathi"]
+#print(type(abhi))
+#avani = ["business","optional","external"]
+#dad = ['abhi']+['avani']
+
+
+abhishek = {"ag":"ah","set":"object","mutable":"not changeble"}
+print(type(abhishek))
+name = input("enter a name:")
+print(abhishek[name])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
