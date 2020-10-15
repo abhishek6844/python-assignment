778bfed9bbe03e0a3f3b79f0f080660602d698c0
abhishek6844 <71318680+abhishek6844@users.noreply.github.com>
2020-10-15 16:23:34 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,55 @@
+#fruit in each list
+"""fruits=["apple","banana","mango"]
+for x in fruits:
+    print(x)"""
+
+"""loop through letters
+for x in "banana":
+    print(x)"""
+
+#exist the loop when i is "abhi"
+"""employee=["datta","aniket","vishal","abhi"]
+for i in employee:
+      print(i)
+      if i=="abhi":
+          break"""
+
+#the continue statements
+"""fruits=["apple","cherry","banana"]
+for x in fruits:
+    if x=="banana":
+        continue
+    print(x)"""
+
+#using range() function
+"""for x in range(10):
+    print(x)"""
+
+#using start parameter
+"""for i in range(3,9):
+    print(i)"""
+
+#increment the sequence with3 (default is 1)
+"""for x in range(2,15,3):
+    print(x)"""
+
+
+#else in for loop
+#ex.print all numbers from 10 to 15 and print a message when the loop has ended
+"""for x in range(10,16):
+    print(x)
+else:
+    print("loop ended")"""
+
+
+#nested loop
+#ex.print each adjective for every fruit:
+"""adj=["red","yellow","orange","green"]
+fruits=["apple","per","lemon","papaya"]
+for x in adj:
+    for y in fruits:
+        print(x,y)"""
+
+#the pass statement
+"""for x in [0,1,2,3]:
+    pass"""
